72dfb9d2ad05972c78240ceb9f8021b751739117
Shiv <mahapatrashiv05@gmail.com>
2024-08-28 00:02:02 +0530

    Data Injestion Done

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from src.exception import CustomException
+
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+#Creating path eg: artifact/train.csv...
+@dataclass
+class DataIngestionConfig:
+    train_data_path : str = os.path.join('artifact','train.csv')
+    test_data_path : str = os.path.join('artifact','test.csv')
+    raw_data_path : str = os.path.join('artifact','data.csv')
+
+
+# Injecting the data to the path created above
+class DataInjestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info("Entered data ingestion process")
+
+        try:
+            df = pd.read_csv("data/cardekho_dataset.csv",index_col=[0])
+            logging.info("reading the data and converting it into dataframe")
+
+            # os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train and Test split initialised")
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=45)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index =False,header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestio of data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+if __name__ == "__main__":
+    
+    obj = DataInjestion()
+
+    train_data , test_data = obj.initiate_data_ingestion()
