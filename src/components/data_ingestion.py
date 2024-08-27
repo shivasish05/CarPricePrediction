29d419f0ec2e3f541c4968799d5753fe3b64a4c6
Shiv <mahapatrashiv05@gmail.com>
2024-08-28 00:17:18 +0530

    Data ingestion Update

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,15 +1,14 @@
 import os
 import sys
 from src.exception import CustomException
-
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
+from dataclasses import dataclass # use to create class variable
 
 
-#Creating path eg: artifact/train.csv...
+# Creating a path eg; 'artifacts/train.csv'.....
 @dataclass
 class DataIngestionConfig:
     train_data_path : str = os.path.join('artifact','train.csv')
@@ -17,40 +16,34 @@ class DataIngestionConfig:
     raw_data_path : str = os.path.join('artifact','data.csv')
 
 
-# Injecting the data to the path created above
-class DataInjestion:
+# Injecting data to tha path created above
+class DataIngestion:
     def __init__(self):
-        self.ingestion_config = DataIngestionConfig()
-    
+        self.ingestion_config =DataIngestionConfig()
+
     def initiate_data_ingestion(self):
-        logging.info("Entered data ingestion process")
-
+        logging.info('Entered data ingestion method')
         try:
-            df = pd.read_csv("data/cardekho_dataset.csv",index_col=[0])
-            logging.info("reading the data and converting it into dataframe")
+            df = pd.read_csv('data/cardekho_dataset.csv',index_col=[0]) 
+            logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok = True)  
+            df.to_csv(self.ingestion_config.raw_data_path,index = False, header=True)
 
-            logging.info("Train and Test split initialised")
-            train_set,test_set = train_test_split(df,test_size=0.2,random_state=45)
-
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header = True)
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+            logging.info("Train test split initioated")
+            train_set, test_set = train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.ingestion_config.train_data_path,index = False , header = True)
+            test_set.to_csv(self.ingestion_config.test_data_path,index = False , header = True)
 
             logging.info("Ingestion of data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
+                self.ingestion_config.test_data_path,
             )
 
         except Exception as e:
-            raise CustomException(e,sys)
-        
-
+               raise CustomException(e,sys)
 if __name__ == "__main__":
-    
-    obj = DataInjestion()
-
-    train_data , test_data = obj.initiate_data_ingestion()
+     obj = DataIngestion()
+     train_data, test_data = obj.initiate_data_ingestion()
