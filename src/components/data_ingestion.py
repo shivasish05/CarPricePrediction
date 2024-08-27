b364396e1ed2b3ecc75564f8aac0be831037d0b3
Shiv <mahapatrashiv05@gmail.com>
2024-08-28 00:08:46 +0530

    Data Injestion Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -35,10 +35,10 @@ class DataInjestion:
             logging.info("Train and Test split initialised")
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=45)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index =False,header = True)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header = True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Ingestio of data is completed")
+            logging.info("Ingestion of data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
